91f2c20b5a818ff9b916689e835d17250580430f
Lucas <lucas0129834765@gmail.com>
2022-09-25 04:14:52 -0300

    commit

--- EntendendoMetodos/src/EntendendoMetodos/Emprestimo.java
+++ EntendendoMetodos/src/EntendendoMetodos/Emprestimo.java
@@ -1,3 +1,4 @@
+package EntendendoMetodos;
 import java.util.Scanner;
 
 public class Emprestimo {
---
+++ EntendendoMetodos/src/Sobrecarga/app.java
@@ -0,0 +1,12 @@
+package Sobrecarga;
+
+public class app {
+    public static void main(String[] args) {
+        
+        System.out.println("Exercicio quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(8, 7, 9);
+        Quadrilatero.area(5f, 5f);
+    }
+}
